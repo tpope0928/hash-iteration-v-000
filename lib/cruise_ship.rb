501cb7da320994e3f3742cb6c730372b588e18ab
Thomas Pope <tpope0928@gmail.com>
2018-08-23 01:21:10 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++
@@ -1,9 +0,0 @@
-def select_winner(passengers)
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
-  end
-end 
-winner  
