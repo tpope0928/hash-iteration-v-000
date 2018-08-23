c11da9bbc6ae1f7b3ef9e41bb0f9efaa69213017
Thomas Pope <tpope0928@gmail.com>
2018-08-23 01:26:47 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,6 +4,10 @@
 #   "Amanda" => 27
 # }
 
-
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
 
 
